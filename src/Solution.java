cc14ec2e67cc6d0e9265454f7c065201cd72f6a9
AleksandrVashchilo <suszycki.alexander@gmail.com>
2023-02-12 21:29:27 +0100

    First commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Solution {
+public class Solution {
+
+    public static String recursion(String s) {
+        // Базовый случай
+        if (s.length() == 1) {
+            return "YES";
+        }
+        else {
+            if (s.substring(0, 1).equals(s.substring(s.length() - 1, s.length()))) {
+
+                if (s.length() == 2) {
+                    return "YES";
+                }
+                // рекурсивное условие
+                return recursion(s.substring(1, s.length() - 1));
+            }
+            else {
+                return "NO";
+            }
+        }
+    }
 }
